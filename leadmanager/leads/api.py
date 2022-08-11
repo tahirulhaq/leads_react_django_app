9935a814df1aae4a287704b71af82c3013d55174
tahirulhaq <tahirulhaq@github.com>
2022-08-11 17:38:23 +0500

    django token auth

--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -3,17 +3,17 @@ from rest_framework import viewsets, permissions
 from .serializers import LeadSerializer
 
 # Lead Viewset
+# no need to create function of post get put and delete
 
 
 class LeadViewSet(viewsets.ModelViewSet):
-    queryset = Lead.objects.all()
     permission_classes = [
-        permissions.AllowAny,
+        permissions.IsAuthenticated,
     ]
     serializer_class = LeadSerializer
 
-    # def get_queryset(self):
-    #     return self.request.user.leads.all()
+    def get_queryset(self):
+        return self.request.user.leads.all()
 
-    # def perform_create(self, serializer):
-    #     serializer.save(owner=self.request.user)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
